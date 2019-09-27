29c8e6ebdec158be26c71a3e283b9ac61dccd309
Douglas Brandão <38235074+douglasbrandao21@users.noreply.github.com>
2019-09-27 15:57:56 -0300

    Initial commit

---
+++ backend/app/Models/Invite.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use("Model");
+
+class Invite extends Model {
+  /**
+   * Definindo o relacionamento de User - Invite
+   * Este usuário é o que receberá o convite para participar do time.
+   * Portando, a relação user - invite é 1:N.
+   */
+  user() {
+    return this.belongsTo("App/Models/User");
+  }
+
+  team() {
+    return this.belongsTo("App/Models/Team");
+  }
+}
+
+module.exports = Invite;
